a51fe05d35b296ead22aa98634b0e932e1192477
Léo Vitor <157988764+euleogithub@users.noreply.github.com>
2024-06-11 14:17:55 -0300

    Add files via upload

---
+++ Meu portfólio (Site)/script.js
@@ -0,0 +1,7 @@
+let menu = document.querySelector(".menuOtherDisplays")
+let box_menu_links = document.querySelector(".box-menuLinks")
+
+function ativarMenu() {
+    menu.classList.toggle("ativar")
+    box_menu_links.classList.toggle("ativar")
+}
