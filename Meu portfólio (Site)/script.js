0d2b40e8e220510f66db9b9b4eb71f8bba072d10
Léo Vitor <157988764+euleogithub@users.noreply.github.com>
2024-06-11 14:31:57 -0300

    Delete Meu portfólio (Site) directory

--- Meu portfólio (Site)/script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector(".menuOtherDisplays")
-let box_menu_links = document.querySelector(".box-menuLinks")
-
-function ativarMenu() {
-    menu.classList.toggle("ativar")
-    box_menu_links.classList.toggle("ativar")
-}
